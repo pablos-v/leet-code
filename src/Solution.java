0e29a54e32aeaeb66aa9f8d059435b766807c66b
Pablos <pablos@list.ru>
2024-04-08 14:57:10 +0300

    274. H-Index

--- src/Solution.java
+++ src/Solution.java
@@ -1,30 +1,16 @@
-import java.util.HashMap;
-import java.util.Map;
-
-/**
- * You are given a 0-indexed array of integers nums of length n. You are initially positioned at nums[0].
- * <p>
- * Each element nums[i] represents the maximum length of a forward jump from index i.
- * In other words, if you are at nums[i], you can jump to any nums[i + j] where:
- * <p>
- * 0 <= j <= nums[i] and
- * i + j < n
- * Return the minimum number of jumps to reach nums[n - 1]. The test cases are generated
- * such that you can reach nums[n - 1].
- */
 class Solution {
-    public int jump(int[] nums) {
-        int moves = 0;
-        int max = 0;
-        int placeChecker = 0;
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            max = Math.max(max, i + nums[i]);
-            if (i == placeChecker) {
-                moves++;
-                placeChecker = max;
-            }
+    public int hIndex(int[] citations) {
+        int maxHIndex = citations.length;
+        int[] citationFrequency = new int[maxHIndex + 1];
+        for (int index : citations) {
+            if (index >= maxHIndex) citationFrequency[maxHIndex]++;
+            else citationFrequency[index]++;
         }
-        return moves;
+        int totalFrequencyCounter = 0;
+        for (int i = citationFrequency.length - 1; i > 0; i--) {
+            totalFrequencyCounter += citationFrequency[i];
+            if (i <= totalFrequencyCounter) return i;
+        }
+        return 0;
     }
 }
--- test/SolutionTest.java
+++ test/SolutionTest.java
@@ -8,26 +8,13 @@ class SolutionTest {
     Solution s = new Solution();
 
     @Test
-    void testForArr1() {
-        int[] arr = {2, 3, 1, 1, 4};
-        int result = s.jump(arr);
-        System.out.println(result);
-        assert result == 2;
-    }
+    void testMethod() {
+        int[] inputArray = {3,0,6,1,5};
+        int response = s.hIndex(inputArray);
+        int expected = 3;
+        System.out.println(response);
+        assert response == expected;
 
-    @Test
-    void testForArr2() {
-        int[] arr = {1, 2};
-        int result = s.jump(arr);
-        System.out.println(result);
-        assert result == 1;
-    }
-    @Test
-    void testForArr3() {
-        int[] arr = {1, 1, 1, 1};
-        int result = s.jump(arr);
-        System.out.println(result);
-        assert result == 3;
     }
 
 }
